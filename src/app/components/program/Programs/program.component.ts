25da4b1303f9213cc7f3bdf9e3caa0a602d35b71
abdullah415 <abdullahhamdo104@gmail.com>
2021-12-13 15:15:50 +0200

    icon

--- src/app/components/program/Programs/program.component.ts
+++ src/app/components/program/Programs/program.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CollectionData } from 'src/DTO/collection-data';
 import { Program } from 'src/Models/Program';
 import { ProgramService } from 'src/Services/program/Program.service';
@@ -11,12 +11,13 @@ import { ProgramService } from 'src/Services/program/Program.service';
 })
 export class ProgramComponent implements OnInit {
 
-  constructor(private Service:ProgramService ,private route:ActivatedRoute) { }
+  constructor(private Service:ProgramService ,private route:ActivatedRoute,private router:Router) { }
 
 
     //#region Declaration Section
     Programs : CollectionData<Program> = new CollectionData<Program>();
     categoryID:number;
+
     //#endregion
 
     //#region Init Method Section
@@ -32,13 +33,15 @@ export class ProgramComponent implements OnInit {
       this.GetPrograms();
       //#endregion
 
+
       //#region  Filter Program By Category ID
       this.route.queryParams.subscribe(
         (query)=>{
-          this.Service.GetProgramsByCatId( query['id']).subscribe(
+          this.Service.GetProgramsByCatId(query['id']).subscribe(
             (data)=>{
               this.Programs.DataList = data.DataList
               // console.log( this.Programs.DataList);
+
             }
           )
         }
